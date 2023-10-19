e67df5ed901e7a0d51b277712abbc168228cc672
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-10-19 18:28:21 +1000

    feat(clipboard): add check to see if running WSL or not before setting the clipboard and use wsl-clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,15 +3,31 @@
 -- Add any additional options here
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
-vim.g.clipboard = {
-  name = "WslClipboard",
-  copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
-  },
-  paste = {
-    ["+"] = 'pwsh.exe -NoLogo -NoProfile -NonInteractive -Command [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
-    ["*"] = 'pwsh.exe -NoLogo -NoProfile -NonInteractive -Command [Console]::Out.Write($(Get-Clipboard -Raw).Tostring().Replace("`r", ""))',
-  },
-  cache_enabled = 0,
-}
+
+-- Set wsl-clipboard for vim clipboard if running WSL
+-- Check if the current linux kernal is microsoft WSL version
+local function is_wsl()
+  local version_file = io.open("/proc/version", "rb")
+  if version_file ~= nil and string.find(version_file:read("*a"), "microsoft") then
+    version_file:close()
+    return true
+  end
+  return false
+end
+
+-- If current linux is under WSL then use wclip.exe
+-- More info: https://github.com/memoryInject/wsl-clipboard
+if is_wsl() then
+  vim.g.clipboard = {
+    name = "wsl-clipboard",
+    copy = {
+      ["+"] = "wcopy",
+      ["*"] = "wcopy",
+    },
+    paste = {
+      ["+"] = "wpaste",
+      ["*"] = "wpaste",
+    },
+    cache_enabled = true,
+  }
+end
