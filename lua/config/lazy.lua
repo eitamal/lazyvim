674d54d6265d8ba7d94116644f56230114785d56
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-10-19 19:22:18 +1000

    chore(extras): move extras to lazyvim.json to be managed by LazyVim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,11 +15,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "plugins" },
   },
   defaults = {
