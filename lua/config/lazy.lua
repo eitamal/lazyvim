b72a595c0beb5542e35fce847a4a29b579f0cd7e
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-11-03 00:08:10 +1000

    fix: add the import statements for the new directories

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,12 +10,10 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins.languages" },
+    { import = "plugins.ui" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
