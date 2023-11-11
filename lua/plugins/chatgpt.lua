80fd4c54f2badd5598f55ad68711e79e55af62ac
Itamar Lencovsky <4740959+eitamal@users.noreply.github.com>
2023-11-11 20:04:40 +1000

    feat(chatgpt): add commands for Windows and WSL to fetch the OpenAI API key for chatgpt.nvim

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -1,3 +1,11 @@
+local os = require("utils.os")
+
+---@type table<os_types, string>
+local os_api_cmds = {
+  [os.types.WINDOWS] = "op read op://Development/openai-chatgpt.nvim/credential --no-newline",
+  [os.types.WSL] = "/home/pluto/dev/repos/auto-scripts/util/chatgpt-nvim.sh",
+}
+
 local wk = require("which-key")
 wk.register({
   C = {
@@ -26,6 +34,7 @@ return {
       "nvim-telescope/telescope.nvim",
     },
     opts = {
+      api_key_cmd = os_api_cmds[os.os],
       openai_params = {
         model = "gpt-4-1106-preview",
         frequency_penalty = 0.7,
